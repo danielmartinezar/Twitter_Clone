ad8fb9c178a1aaab836d6dd732556a3c1abb9abc
danielking1205 <73305373+danielking1205@users.noreply.github.com>
2022-11-04 17:40:44 -0500

    Update config.js
     database name changed

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ export default {
   APP_PORT: process.env.APP_PORT || 8081,
   DB_URL:
     process.env.DB_URL ||
-    "mongodb+srv://danielmartinezar_:iZKcKcnPbztXDpbs@twitter.e40qjfe.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://danielmartinezar_:iZKcKcnPbztXDpbs@twitter.e40qjfe.mongodb.net/Twitter?retryWrites=true&w=majority",
 };
